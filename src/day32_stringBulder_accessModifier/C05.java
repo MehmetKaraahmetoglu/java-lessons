f08517d153321a13de621331238d3b57e7888cd7
MehmetKaraahmetoglu <mek_memin@hotmail.com>
2023-08-07 21:49:02 +0200

    Java lessons are added.

--- src/day32_stringBulder_accessModifier/C05.java
+++ src/day32_stringBulder_accessModifier/C05.java
@@ -1,2 +1,9 @@
-package day32_stringBulder_accessModifier;public class C05 {
+package day32_stringBulder_accessModifier;
+
+public class C05 {
+
+    private int privateSayi;
+    int defaultSayi;
+    protected int protectedSayi;
+    public int publicSayi;
 }
--- src/day32_stringBulder_accessModifier/C06_Runner.java
+++ src/day32_stringBulder_accessModifier/C06_Runner.java
@@ -1,2 +1,23 @@
-package day32_stringBulder_accessModifier;public class C06_Runner {
+package day32_stringBulder_accessModifier;
+
+public class C06_Runner {
+    public static void main(String[] args) {
+
+        C05 obj = new C05();
+
+        // System.out.println(obj.privateSayi);
+        // Private class üyelerine  baska classtan ulasma sansimiz yok.
+
+        System.out.println(obj.defaultSayi); // 0
+        obj.defaultSayi=20;
+        System.out.println(obj.defaultSayi); // 20
+
+        System.out.println(obj.protectedSayi); // 0
+        obj.protectedSayi=30;
+        System.out.println(obj.protectedSayi);// 30
+
+        System.out.println(obj.publicSayi);
+        obj.publicSayi=40;
+        System.out.println(obj.publicSayi); //40
+    }
 }
