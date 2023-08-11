95c57f774e3d00a862fff323850931fa22a42168
MehmetKaraahmetoglu <mek_memin@hotmail.com>
2023-08-11 21:50:17 +0200

    Java lessons are added.

--- src/day36_inheritanceDataTypeKullanimi/JParentClass.java
+++ src/day36_inheritanceDataTypeKullanimi/JParentClass.java
@@ -1,2 +1,12 @@
-package day36_inheritanceDataTypeKullanimi;public class JParentClass {
+package day36_inheritanceDataTypeKullanimi;
+
+public class JParentClass {
+
+    public void method1(){
+        System.out.println("Parent class method1");
+
+    }
+    public  void method2(){
+        System.out.println("parent class method2");
+    }
 }
--- src/day36_inheritanceDataTypeKullanimi/KChildClass.java
+++ src/day36_inheritanceDataTypeKullanimi/KChildClass.java
@@ -1,2 +1,30 @@
-package day36_inheritanceDataTypeKullanimi;public class KChildClass {
+package day36_inheritanceDataTypeKullanimi;
+
+public class KChildClass extends JParentClass{
+    public void method1(){
+        System.out.println("Child class method1");
+    }
+
+    @Override
+    public void method2() {
+        super.method2();
+
+        /*
+        @Override notasyonu override method ile overriding method'u
+        birbirine baglar
+        eger overriden method silinirse
+        CTE verir.(Parent classinda ki method2 silinirse
+        CTE verir yani.)
+
+        Kullanmak zorunda degiliz
+        ama iki method'u bagimli yapmak istiyorsak kullanilabilir.
+
+        Overriding method varken
+        overridden method calismaz
+
+        EGER her iki methodu birden calistirmak istersek
+        child class'daki overriding method'a
+        super.method(); ekleyebiliriz
+         */
+    }
 }
